52cfc3df128f7aa2d47243a09c5b74a763389d9e
Sergei Tishkov <s.tishkov@qcentris.com>
2021-05-12 23:31:56 +0300

    constructor is added to VideoModel

--- src/dataModels/VideoModel.ts
+++ src/dataModels/VideoModel.ts
@@ -1,10 +1,17 @@
 export default class VideoModel {
-  /**Prepared Blob from arrayBuffer and type */
   blob?: Blob;
-
-  /**URL that is prepared by window object and can be used in src */
   objectUrl?: string;
-
-  /**Stringified thumbnails of each 0.25 seconds of the video, can be used in src */
   thumbnails?: string[];
+
+  /**
+   * Constructor of VideoModel
+   * @param blob Blob that contains the video 
+   * @param objectUrl URL that is prepared by window object and can be used in src
+   * @param thumbnails Stringified thumbnails of each 0.25 seconds of the video, can be used in src
+   */
+  constructor(blob: Blob, objectUrl: string, thumbnails: string[]) {
+    this.blob = blob;
+    this.objectUrl = objectUrl;
+    this.thumbnails = thumbnails;
+  }
 }
