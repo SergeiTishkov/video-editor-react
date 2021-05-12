65094af187c4c4c90cd8558536441738afcc978d
Sergei Tishkov <s.tishkov@qcentris.com>
2021-05-12 22:30:53 +0300

    extractFramesFromVideo function is added to the project

---
+++ src/Utils/extractFramesFromVideo.js
@@ -0,0 +1,44 @@
+export async function extractFramesFromVideo(videoBlob, imageInterval = 0.25) {
+  return new Promise(async resolve => {
+    let videoObjectUrl = URL.createObjectURL(videoBlob);
+    let video = document.createElement("video");
+
+    let seekResolve;
+    video.addEventListener("seeked", async function () {
+      if (seekResolve) {
+        seekResolve();
+      }
+    });
+
+    video.src = videoObjectUrl;
+
+    // workaround chromium metadata bug (https://stackoverflow.com/q/38062864/993683)
+    while ((video.duration === Infinity || isNaN(video.duration)) && video.readyState < 2) {
+      await new Promise(r => setTimeout(r, 1000));
+      video.currentTime = 10000000 * Math.random();
+    }
+
+    let duration = video.duration;
+
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext("2d");
+    let [w, h] = [video.videoWidth, video.videoHeight];
+    canvas.width = w;
+    canvas.height = h;
+
+    let frames = [];
+    let currentTime = 0;
+
+    while (currentTime < duration) {
+      video.currentTime = currentTime;
+      await new Promise(r => (seekResolve = r));
+
+      context.drawImage(video, 0, 0, w, h);
+      let base64ImageData = canvas.toDataURL();
+      frames.push(base64ImageData);
+
+      currentTime += imageInterval;
+    }
+    resolve(frames);
+  });
+}
