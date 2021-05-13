c8eadd06d7bdd901e4848c95109947ddc750059d
Sergei Tishkov <s.tishkov@qcentris.com>
2021-05-14 00:48:49 +0300

    VideoPlayerComponent - added onTimeUpdate prop event handler

--- src/Components/VideoPlayerComponent/VideoPlayerComponent.jsx
+++ src/Components/VideoPlayerComponent/VideoPlayerComponent.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import styles from "./VideoPlayerComponent.module.scss";
 import classNames from "classnames";
 
-const VideoPlayerComponent = ({ videoSrc }) => {
+const VideoPlayerComponent = ({ videoSrc, onTimeUpdate }) => {
   const [paused, setPaused] = React.useState(true);
   const videoRef = React.useRef();
 
   const togglePlayPause = () => {
     paused ? videoRef.current.play() : videoRef.current.pause();
-    
+
     setPaused(!paused);
   };
 
   return (
     <div className={classNames(styles["component-container"], videoSrc ? "" : styles["no-content"])}>
       <div className={styles["video-container"]}>
-        <video ref={videoRef} src={videoSrc} />
+        <video ref={videoRef} src={videoSrc} onTimeUpdate={e => onTimeUpdate && onTimeUpdate(e, videoRef.current)} />
       </div>
       <div className={styles["controls-container"]}>
         <button onClick={togglePlayPause}>{paused ? "play" : "pause"}</button>
