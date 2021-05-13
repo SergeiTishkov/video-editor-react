01a3dff337c170697471f3b7092e1773a2f18101
Sergei Tishkov <s.tishkov@qcentris.com>
2021-05-14 00:20:25 +0300

    controls attribute is removed from the <video> tag and replased with simple play/pause btn

--- src/Components/VideoPlayerComponent/VideoPlayerComponent.jsx
+++ src/Components/VideoPlayerComponent/VideoPlayerComponent.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import styles from "./VideoPlayerComponent.module.scss";
+import classNames from "classnames";
 
-const VideoPlayerComponent = ({videoSrc}) => {
+const VideoPlayerComponent = ({ videoSrc }) => {
+  const [paused, setPaused] = React.useState(true);
+  const videoRef = React.useRef();
+
+  const togglePlayPause = () => {
+    paused ? videoRef.current.play() : videoRef.current.pause();
+    
+    setPaused(!paused);
+  };
+
   return (
-    <div className={styles["container"]}>
-      <video src={videoSrc} {...(videoSrc ? { controls: true } : {})} />
+    <div className={classNames(styles["component-container"], videoSrc ? "" : styles["no-content"])}>
+      <div className={styles["video-container"]}>
+        <video ref={videoRef} src={videoSrc} />
+      </div>
+      <div className={styles["controls-container"]}>
+        <button onClick={togglePlayPause}>{paused ? "play" : "pause"}</button>
+      </div>
     </div>
   );
 };
